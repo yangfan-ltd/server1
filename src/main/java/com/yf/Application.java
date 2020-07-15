4458d9eff0eb371f65824ab648a1973f124772e1
tom_glb <6571553+yangfan-ltd@user.noreply.gitee.com>
2020-07-16 00:51:40 +0800

    server commit4

---
+++ src/main/java/com/yf/Application.java
@@ -0,0 +1,18 @@
+package com.yf;
+
+/**
+ * @author ：author
+ * @date ：Created in 2020-07-11 19:55
+ * @description：
+ * @modified By：
+ * @version:
+ */
+public class Application {
+    public static void main(String[] args) {
+
+        System.out.println("success1");
+        System.out.println("success2");
+        System.out.println("success3");
+        System.out.println("success4");
+    }
+}
