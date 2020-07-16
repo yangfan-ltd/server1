a42f17aad0b0238ec38d2ce1dbcc67a9a45b1559
yangfan <xiaolu.yang@newtouch.com>
2020-07-16 22:57:09 +0800

    改变14

--- src/main/java/com/yf/Application.java
+++ src/main/java/com/yf/Application.java
@@ -23,5 +23,6 @@ public class Application {
         System.out.println("success11");
         System.out.println("success12");
         System.out.println("success13");
+        System.out.println("success14");
     }
 }
